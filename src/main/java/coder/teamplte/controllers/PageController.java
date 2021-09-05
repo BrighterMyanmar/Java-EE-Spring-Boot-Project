e4f53ec5962da0e631801c4192820753e4391134
Brighter Myanmar <brightermyanmar@gmail.com>
2021-09-05 17:04:27 +0630

    Sub Categories Done

--- src/main/java/coder/teamplte/controllers/PageController.java
+++ src/main/java/coder/teamplte/controllers/PageController.java
@@ -1,72 +1,14 @@
 package coder.teamplte.controllers;
-import coder.teamplte.models.Category;
-import coder.teamplte.models.daos.CategoryDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/cats")
 public class PageController {
-    static String UPLOAD_FOLDER = "src/main/resources/static/uploads/";
-
-    @Autowired
-    CategoryDao categoryDao;
 
     @GetMapping
-    public String index(Model model){
-        model.addAttribute("cats",categoryDao.findAll());
-        return "cats/index";
-    }
-
-    @GetMapping(value="/create")
-    public String create(){
-        return "cats/create";
-    }
-
-    @PostMapping(value="/store")
-    public String store(MultipartFile file, @RequestParam String name){
-       String filename =  saveFile(file);
-       Category cat = new Category(name,filename);
-       categoryDao.save(cat);
-       return "redirect:";
-    }
-
-    @GetMapping(value="/edit/{id}")
-    public String edit(Model model,@PathVariable int id){
-        model.addAttribute("cat",categoryDao.findById(id).orElse(null));
-        return "cats/edit";
-    }
-
-    @PostMapping(value="/update/{id}")
-    public String update(@PathVariable int id,@RequestParam("file") MultipartFile file,@RequestParam String name){
-        Category cat = categoryDao.findById(id).orElse(null);
-        if(file.getSize() > 0){
-            cat.setImage(saveFile(file));
-        }
-        cat.setName(name);
-        categoryDao.save(cat);
-        return "redirect:/cats";
-    }
-
-    public String saveFile(MultipartFile file){
-        String filename = file.getOriginalFilename();
-        try{
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_FOLDER + filename);
-            Files.write(path,bytes);
-        }catch(IOException e){
-            System.out.println("File Write Error");
-        }
-        return filename;
+    public String index() {
+        return "index";
     }
 
 }
--- src/main/java/coder/teamplte/controllers/SubcatController.java
+++ src/main/java/coder/teamplte/controllers/SubcatController.java
@@ -7,55 +7,85 @@ import coder.teamplte.models.daos.SubcatDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/subcats")
 public class SubcatController {
-
+    static String UPLOAD_FOLDER = "src/main/resources/static/uploads/";
     @Autowired
     SubcatDao subcatDao;
     @Autowired
     CategoryDao categoryDao;
 
     @GetMapping
-    public String index(Model model){
-        model.addAttribute("subcats",subcatDao.findAll());
+    public String index(Model model) {
+        model.addAttribute("subcats", subcatDao.findAll());
         return "subcats/index";
     }
 
-    @GetMapping(value="/store")
-    @ResponseBody
-    public String store(){
-        Category cat = categoryDao.findById(1).orElse(null);
+    @GetMapping(value = "/create")
+    public String create(Model model) {
+        model.addAttribute("cats", categoryDao.findAll());
+        return "subcats/create";
+    }
+
+    @PostMapping(value = "/store")
+    public String store(@RequestParam("file") MultipartFile file, @RequestParam String name, @RequestParam int category_id) {
+        Category cat = categoryDao.findById(category_id).orElse(null);
         Subcat subcat = new Subcat();
-        subcat.setName("Computer");
-        subcat.setImage("banner2.jpg");
+        subcat.setName(name);
+        subcat.setImage(saveFile(file));
         subcat.setCategory(cat);
         subcatDao.save(subcat);
-        return "Save Success";
+        return "redirect:";
     }
 
-    @GetMapping(value="/update")
-    @ResponseBody
-    public String update(){
-//        Category cat = categoryDao.findById(5).orElse(null);
-        Subcat subcat = subcatDao.findById(7).orElse(null);
+    @GetMapping(value = "/edit/{id}")
+    public String edit(Model model, @PathVariable int id) {
+        model.addAttribute("cats", categoryDao.findAll());
+        model.addAttribute("subcat", subcatDao.findById(id).orElse(null));
+        return "subcats/edit";
+    }
 
-        subcat.setName("Phone");
+    @PostMapping(value = "/update/{id}")
+    public String update(@PathVariable int id, @RequestParam String name,
+                         @RequestParam int category_id, @RequestParam("file") MultipartFile file) {
+        Category cat = categoryDao.findById(category_id).orElse(null);
+        Subcat subcat = subcatDao.findById(id).orElse(null);
+        if (file.getSize() > 0) {
+            subcat.setImage(saveFile(file));
+        }
+        subcat.setName(name);
+        subcat.setCategory(cat);
         subcatDao.save(subcat);
 
-        return "Sub Category Updated";
+        return "redirect:/subcats";
     }
 
-    @GetMapping(value="/delete")
-    @ResponseBody
-    public String delete(){
-        Subcat subcat = subcatDao.findById(3).orElse(null);
+    @GetMapping(value = "/delete/{id}")
+    public String delete(@PathVariable int id) {
+        Subcat subcat = subcatDao.findById(id).orElse(null);
         subcatDao.delete(subcat);
-        return "Sub Category Deleted!";
+        return "redirect:/subcats";
+    }
+
+    public String saveFile(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_FOLDER + filename);
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            System.out.println("File Write Error");
+        }
+        return filename;
     }
 
 }
